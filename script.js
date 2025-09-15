4dfe4776f77e76a8597be0aa0cff91c42885a2b6
Narek <nmalkhasyan35@gmail.com>
2025-09-15 11:14:13 +0400

    add javascript homework file

---
+++ script.js
@@ -0,0 +1,76 @@
+/*1.
+let newUser="Narek";
+
+
+2.ինչ կունենանք console-ում
+let name="Ilya";
+console.log(`hello ${name}`);
+ktpi hello Ilya 
+
+3.
+let userAge="шестнадцать";
+console.log(userAge);
+
+let userName=12345;
+console.log(userName);
+
+let isOnline="yes";
+console.log(isOnline);
+
+let score=true;
+console.log(score);
+
+Ենթադրում եմ,որ isOnline-ը պիտի լինի boolean տիպի,ոչ թե string.
+Մնացածի դեպքում էլ նույն բաննա․
+Բայց բոլորն են աշխատում․
+
+
+4.
+let a=1;
+let b=1;
+
+let c=++a;
+let d=b++;
+
+console.log(a);
+console.log(b);
+
+console.log(d);
+console.log(c);
+
+Պատասղանը կլինի՝ 2,2,1,2
+
+
+5.
+a.
+1."10";
+2.(-1);
+3.true+false=1+0=1;
+4.6/"3"=6/3=2;
+5."2"*"3"=2*3=6;
+6."9px";
+7."$45";
+8.2;
+9.NaN;
+10."-95";
+11.-14;
+12.1;
+13.NaN;
+
+b.
+1.5>4,yes /true;
+2.false;
+3.true;
+4.true;
+5.false;
+6.Chgitem;
+7.Chgitem;
+
+c.Barda,kuzem mi hat qnnarkenq lsaranum
+d.Barda,kuzem mi hat qnnarkenq lsaranum
+
+
+
+
+
+*/
