71ebaeec767ba8ba0be9857e96fa00584b70605e
Narek <nmalkhasyan35@gmail.com>
2025-09-29 10:07:43 +0400

    Homework 5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let person = {
+/*let person = {
     name: "Narek",
     age: 24,
     city: "Erevan"
@@ -9,4 +9,22 @@ alert(person);
 
 delete person.city;
 
-alert("age" in person);
+alert("age" in person);*/
+
+function removeUndefinedProps(obj) {
+    for (const key in obj) {
+      if (obj[key] === undefined) delete obj[key];
+    }
+  }
+  
+  const obj = {
+    key1: 'hello',
+    key2: undefined,
+    key3: 123,
+    key4: undefined
+  };
+  
+  removeUndefinedProps(obj);
+  console.log(obj);
+  // Արդյունք: { key1: 'hello', key3: 123 }
+  
